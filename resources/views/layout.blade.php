553607240bf80d29abab3d7e4e57a617185e023b
Jason <jsfernandes83@gmail.com>
2022-02-03 01:23:18 -0700

    Defect:fixed incorrect app name in layout.blade.php

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Canadian Couch Potatoe</title>
+        <title>Larabot</title>
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <link rel="stylesheet" href="/css/app.css">
         <link href="https://fonts.googleapis.com/css?family=Arimo|Roboto+Condensed:400,700,900" rel="stylesheet">
